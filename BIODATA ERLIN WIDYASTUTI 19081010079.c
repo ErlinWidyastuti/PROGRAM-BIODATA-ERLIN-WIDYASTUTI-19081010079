c99429c90afa321274d948a901ec5ead356c10de
ErlinWidyastuti <erlinwidyastuti15@gmail.com>
2020-09-22 10:57:34 +0700

    first commit

---
+++ BIODATA ERLIN WIDYASTUTI 19081010079.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+
+int main(int argc, char *argv[]) 
+{
+	system("color E0");
+	printf("||======================================================||\n");
+	printf("||\t\t\t BIODATA \t\t\t||\n");
+	printf("||======================================================||\n");
+	printf("||  Nama Lengkap\t : Erlin Widyastuti\t\t||\n");
+	printf("||  Nama Panggilan\t : Erlin\t\t\t||\n");
+	printf("||  NPM\t\t\t : 19081010079\t\t\t||\n");
+	printf("||  TTL\t\t\t : Surabaya, 1 Mei 2001\t\t||\n");
+	printf("||  Jenis Kelamin\t : Perempuan\t\t\t||\n");
+	printf("||  Alamat\t\t : Simomulyo Baru 4D/9 Surabaya\t||\n");
+	printf("||  Agama\t\t : Islam\t\t\t||\n");
+	printf("||  Kewarganegaraan\t : Indonesia\t\t\t||\n");
+	printf("||  Status\t\t : Pelajar\t\t\t||\n");
+	printf("||  Hobi\t\t : Menyanyi\t\t\t||\n");
+	printf("||  Email\t\t : erlinwidyastuti15@gmail.com\t||\n");
+	printf("||  No.Hp\t\t : 082141261905\t\t\t||\n");
+	printf("||======================================================||\n\n");
+	return 0;
+}
